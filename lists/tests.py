cb8b5d7f9ae16bb48e768adc7e4405eadbe10018
Bebo Elhosary <beboelhosary@hotmail.com>
2018-06-17 23:04:48 -0700

    First unit test and url mapping, dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,12 +1,23 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
 # functional test from users's persepctive
 # unittest from programmer's perspective
 # TestCase is an augmented version of unittest.TestCase
 # run file using python manage.py test
 
-class SmokeTest(TestCase):
+# Django's main job is to decide what to do whe a user asks for a particular
+# URL on our site. Django's workflow is like this:
+#
+# 1) An HTTP request comes in for a particular URL
+# 2) Django uses some rules to decide which view function should deal with the
+# request (referred to as resolving the URL)
+# 3) The view function processes the request and returns an HTTP response
 
-	def test_bad_maths(self):
-		self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+
+	def test_root_url_resolves_to_home_page_view(self):
+		found = resolve('/')
+		self.assertEqual(found.func, home_page)
 		

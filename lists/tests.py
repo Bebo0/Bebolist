b94902f7bbedda2b07119f2517b27759ea5781d3
Bebo Elhosary <beboelhosary@hotmail.com>
2018-06-19 20:40:13 -0700

    Front page HTML now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -19,18 +19,18 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/')
-		self.assertEqual(found.func, home_page)
+	# def test_root_url_resolves_to_home_page_view(self):
+	# 	found = resolve('/')
+	# 	self.assertEqual(found.func, home_page)
 		
 	def test_home_page_returns_correct_html(self):
 		# request = httpRequest() # what Django will see when a user's browser asks for a page
 		response = self.client.get('/') # passes the URL we want to test
 		#response = home_page(request)
 
-		html = response.content.decode('utf8') # gets raw bytes then converts them to HTML
-		self.assertTrue(html.startswith('<html>'))
-		self.assertIn('<title>To-Do lists</title>', html)
-		self.assertTrue(html.endswith('</html>'))
+		# html = response.content.decode('utf8') # gets raw bytes then converts them to HTML
+		# self.assertTrue(html.startswith('<html>'))
+		# self.assertIn('<title>To-Do lists</title>', html)
+		# self.assertTrue(html.endswith('</html>'))
 
-		self.assertTemplateUsed(response, 'wrong.html') # checks which template (we want 'home.html') was used to render a response
+		self.assertTemplateUsed(response, 'home.html') # checks which template (we want 'home.html') was used to render a response

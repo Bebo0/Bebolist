5166ac554e136ee21aca735a4df7565545657b68
Bebo Elhosary <beboelhosary@hotmail.com>
2018-06-22 01:47:17 -0700

    Remove explicit wait. Add helper for waiting

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+
 from django.test import LiveServerTestCase
+
 import time
 import unittest
 
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -13,6 +17,19 @@ class NewVisitorTest(LiveServerTestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def wait_for_row_in_list_table(self, row_text):
+		start_time = time.time()
+		while True:
+			try:
+				table = self.browser.find_element_by_id('id_list_table')
+				rows = table.find_elements_by_tag_name('tr')
+				self.assertIn(row_text, [row.text for row in rows])
+				return
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+
+					raise e
+				time.sleep(0.5)
 	def test_can_start_a_list_and_retrieve_it_later(self):
 
 		# Edith has heard about a cool new online to-do app. She goes
@@ -41,8 +58,8 @@ class NewVisitorTest(LiveServerTestCase):
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER) # Keys class lets us send special keys like Enter
-		time.sleep(1) # explicit wait for browser to refresh
-
+		# time.sleep(1) # explicit wait for browser to refresh
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 		
 
 		# There is still a text box inviting her to add another item. She
@@ -51,9 +68,12 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
+		
 
 		# The page updates again, and now shows both items on her list
+
+		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')

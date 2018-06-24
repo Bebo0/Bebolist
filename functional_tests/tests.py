da252a608f98d12369c5454e11b448826de854f9
Bebo Elhosary <beboelhosary@hotmail.com>
2018-06-23 17:05:34 -0700

    Add FT for multiple users functionality

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -30,7 +30,7 @@ class NewVisitorTest(LiveServerTestCase):
 
 					raise e
 				time.sleep(0.5)
-	def test_can_start_a_list_and_retrieve_it_later(self):
+	def test_can_start_a_list_for_one_user(self):
 
 		# Edith has heard about a cool new online to-do app. She goes
 		# to check out its homepage
@@ -90,6 +90,43 @@ class NewVisitorTest(LiveServerTestCase):
 		# Satisfied, she goes back to sleep
 
 		self.fail('Finish the test!')
+
+	def test_multiple_users_can_start_lists_at_different_urls(self):
+		# Edith starts a new to-do list
+		self.browser.get(self.live_server_url)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+		# She notices that her list has a unique URL
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url, '/lists/.+') # checks whether a string matches a regular expression
+
+		# Now a user, Francis, comes along to the site.
+
+		## We use a new browser session to make sure that no information
+		## of Edith's is coming through from cookies etc.
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+		# Francis visits the home page.  There is no sign of Edith's list
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
+
+		# Francis starts a new list by entering a new item. He is less interesting than Edith...
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy milk')
+
+		# Again, there is no trace of Edith's list
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('Buy milk', page_text)
+
+    	# Satisfied, they both go back to sleep
 
 if __name__ == '__main__':  
 

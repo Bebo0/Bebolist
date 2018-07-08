88f9abc6140585b5602002582695c6d8a93f7e42
Bebo Elhosary <beboelhosary@hotmail.com>
2018-07-07 22:21:44 -0700

    Split unit tests into two files

---
+++ lists/tests/test_models.py
@@ -0,0 +1,52 @@
+from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpRequest
+
+from lists.views import home_page
+from lists.models import Item, List
+
+# functional test from users's persepctive
+# unittest from programmer's perspective
+# TestCase is an augmented version of unittest.TestCase
+# run file using python manage.py test
+
+# Django's main job is to decide what to do whe a user asks for a particular
+# URL on our site. Django's workflow is like this:
+#
+# 1) An HTTP request comes in for a particular URL
+# 2) Django uses some rules to decide which view function should deal with the
+# request (referred to as resolving the URL)
+# 3) The view function processes the request and returns an HTTP response
+
+class ListAndItemModelTest(TestCase):
+
+	def test_saving_and_retrieving_items(self):
+		list_ = List()
+		list_.save()
+
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.list = list_
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.list = list_
+		second_item.save()
+
+		saved_list = List.objects.first()
+		self.assertEqual(saved_list, list_)
+
+		save_items = Item.objects.all() # QuerySet which is a list-like
+		self.assertEqual(save_items.count(), 2)
+
+		first_saved_item = save_items[0]
+		second_saved_item = save_items[1]
+		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(first_saved_item.list, list_)
+		self.assertEqual(second_saved_item.text, 'Item the second')
+		self.assertEqual(second_saved_item.list, list_)
+
+	# def test_only_saves_items_when_necessary(self):
+	# 	self.client.get('/')
+	# 	self.assertEqual(Item.objects.count(), 0)
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -58,40 +58,6 @@ class HomePageTest(TestCase):
 
 	# 	self.assertIn('itemey 1', response.content.decode())
 	# 	self.assertIn('itemey 2', response.content.decode())
-		
-
-class ListAndItemModelTest(TestCase):
-
-	def test_saving_and_retrieving_items(self):
-		list_ = List()
-		list_.save()
-
-		first_item = Item()
-		first_item.text = 'The first (ever) list item'
-		first_item.list = list_
-		first_item.save()
-
-		second_item = Item()
-		second_item.text = 'Item the second'
-		second_item.list = list_
-		second_item.save()
-
-		saved_list = List.objects.first()
-		self.assertEqual(saved_list, list_)
-
-		save_items = Item.objects.all() # QuerySet which is a list-like
-		self.assertEqual(save_items.count(), 2)
-
-		first_saved_item = save_items[0]
-		second_saved_item = save_items[1]
-		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-		self.assertEqual(first_saved_item.list, list_)
-		self.assertEqual(second_saved_item.text, 'Item the second')
-		self.assertEqual(second_saved_item.list, list_)
-
-	# def test_only_saves_items_when_necessary(self):
-	# 	self.client.get('/')
-	# 	self.assertEqual(Item.objects.count(), 0)
 
 class ListViewTest(TestCase):
 

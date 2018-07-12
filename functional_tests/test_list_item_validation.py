917124c5496dc102c232088e61b69534d5a73c59
Bebo Elhosary <beboelhosary@hotmail.com>
2018-07-12 00:48:27 -0700

    Use form in all views, back to working state

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -28,26 +28,34 @@ class ItemValidationTest(FunctionalTest):
 		## We'll use a CSS class called .has-error to mark our error text
 		## lambda example: myfn = lambda x: x + 1
 		## myfn(2) = 3
-		self.wait_for(lambda: self.assertEqual(
-			self.browser.find_element_by_css_selector('.has-error').text, 
-			"You can't have an empty list item"))  
 
-		# She tries again with some text for the item, which now works
+
+		# The browser intercepts the request, and doesn not load the list page
+		## We check whether it's a valid input by checking the CSS pseudoselector :invalid,
+		## which the browser applies to any HTML5 input that has invalid input
+		self.wait_for(lambda: self.browser.find_element_by_css_selector('#id_text:invalid'))
+
+		# She starts typing some text for the new item and the error disappears
 		self.get_item_input_box().send_keys('Buy milk')
+		self.wait_for(lambda: self.browser.find_element_by_css_selector('#id_text:valid'))
+
+		# And she can submit it succesfully
 		self.get_item_input_box().send_keys(Keys.ENTER)
 		self.wait_for_row_in_list_table('1: Buy milk')
 
 		# Perversely, she now decides to submit a second blank list item
 		self.get_item_input_box().send_keys(Keys.ENTER)
 
-		# She receives a similar warning on the list page
-		self.wait_for(lambda: self.assertEqual(
-		    self.browser.find_element_by_css_selector('.has-error').text,
-		    "You can't have an empty list item"
-		))
+		# Again the browser will not comply
+		self.wait_for(lambda: self.browser.find_element_by_css_selector('#id_text:invalid'))
+
 
 		# And she can correct it by filling some text in
 		self.get_item_input_box().send_keys('Make tea')
 		self.get_item_input_box().send_keys(Keys.ENTER)
+		self.wait_for(lambda: self.browser.find_element_by_css_selector('#id_text:valid'))
+
 		self.wait_for_row_in_list_table('1: Buy milk')
+		
 		self.wait_for_row_in_list_table('2: Make tea')
+

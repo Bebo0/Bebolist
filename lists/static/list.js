04c62d952c756fc7c48a4c264c7555751c9f84c9
Bebo Elhosary <beboelhosary@hotmail.com>
2018-07-19 10:29:36 +0200

    Add namespace object and make initalize() an attribute of it

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,4 +1,7 @@
-var initialize = function () {
+window.Superlists = {}; // we declare an object as property of the "window" global
+// giving it a name that we think no one else is likely to use
+window.Superlists.initialize = function () { 
+	// we make our initialize function an attribute of the namespace object
 	$('input[name="text"]').on('keypress', function () {
 		$('.has-error').hide();
 	});

829843038ae59c48d278efb927fd7eb2e757b2ba
Bebo Elhosary <beboelhosary@hotmail.com>
2018-08-11 13:52:18 -0700

    Add models.py tests and 2 new models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class User(models.Model):
+	email = models.EmailField(primary_key=True) # We only need this but Django complains about the underneath attributes
+	# We make the emails the primary key instead of the auto generated IDs (1, 2, 3, ...)
+	REQUIRED_FIELDS = []
+	USERNAME_FIELD = 'email'
+	is_anonymous = False
+	is_authenticated = True
+
+class Token(models.Model):
+	email = models.EmailField()
+	uid = models.CharField(default=uuid.uuid4, max_length=40) # Python comes with a module designed specifically
+	# for generating unique IDs called "uuid" for "universally unique id". Better than using random.

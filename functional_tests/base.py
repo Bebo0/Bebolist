e5b3116d74fd5cb715b285a860d4e1f686c391ba
Bebo Elhosary <beboelhosary@hotmail.com>
2018-08-19 11:12:43 -0700

    Add custom @wait decorator to reduce duplication

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -39,31 +39,36 @@ class FunctionalTest(StaticLiveServerTestCase):
 					raise e
 				time.sleep(0.5)
 
+	@wait
 	def wait_for(self, fn):
-		start_time = time.time()
-		while True:
-			try:
-				return fn()
-			except (AssertionError, WebDriverException) as e:
-				if time.time() - start_time > MAX_WAIT:
+		return fn()
 
-					raise e
-				time.sleep(0.5)
-
+	"""Our own wait decorator function
+	
+	A decorator is a way of modifying a function; it takes an function as an argument
+	and returns another function as the modified or decorated version
+	"""
+	def wait(fn):
+		def modified_fn(*args, **kwargs): # We specify that modified_fn may take any
+		# arbitrary positional and keyword arguments
+			start_time = time.time()
+			while True:
+				try:
+					return fn(*args, **kwargs) # We pass those same arguments to fn when we call it
+				except (AssertionError, WebDriverException) as e:
+					if time.time() - start_time > MAX_WAIT:
+						raise e
+					time.sleep(0.5)
+		return modified_fn
 
 	def get_item_input_box(self):
 		return self.browser.find_element_by_id('id_text')
 
+	@wait
 	def wait_to_be_logged_in(self, email):
-		self.wait_for(
-			lambda: self.browser.find_element_by_link_text('Log out')
-			)
-		navbar = self.browser.find_element_by_css_selector('.navbar')
-		self.assertIn(email, navbar.text)
+		self.browser.find_element_by_link_text('Log out')
 
+	@wait
 	def wait_to_be_logged_out(self, email):
-		self.wait_for(
-			lambda: self.browser.find_element_by_name('email')
-			)
-		navbar = self.browser.find_element_by_css_selector('.navbar')
-		self.assertNotIn(email, navbar.text)
+		self.browser.find_element_by_name('email')
+
